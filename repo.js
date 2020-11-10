dc835a3c9da95b039864b62380ad589c17ae2e2f
Lmocinemod <73551205+Lmocinemod@users.noreply.github.com>
2020-11-09 21:15:04 -0600

    Initial commit

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+  "contact": "https://twitter.com/Lmocinemod",
+  "id": "LmocinemodPatchRepo",
+  "patches": {
+    "marisa_silent_laser": "Makes Marisa's laser silent in th12 and above."
+  },
+  "servers": [
+    "https://mirrors.thpatch.net/LmocinemodPatchRepo/"
+  ],
+  "title": "Lmocinemod's (Low-Effort) Patch Repository"
+}
