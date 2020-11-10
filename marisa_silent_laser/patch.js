a64b77b1fadc2542134b23605d71e79ed107d302
Lmocinemod <73551205+Lmocinemod@users.noreply.github.com>
2020-11-10 16:55:15 -0600

    Fix server URL

--- marisa_silent_laser/patch.js
+++ marisa_silent_laser/patch.js
@@ -1,7 +1,7 @@
 {
   "id": "marisa_silent_laser",
   "servers": [
-    "https://mirrors.thpatch.net/LmocinemodPatchRepo/marisa_silent_laser"
+    "https://mirrors.thpatch.net/LmocinemodPatchRepo/marisa_silent_laser/"
   ],
   "title": "Makes Marisa's laser silent in th12 and above."
 }
