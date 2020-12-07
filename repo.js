848edd94f9fa64f6716332745b7a9ff15984296d
Lmocinemod <73551205+Lmocinemod@users.noreply.github.com>
2020-12-07 16:12:39 -0600

    Add th17_marisa_silent_missile patch

--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
   "contact": "https://twitter.com/Lmocinemod",
   "id": "LmocinemodPatchRepo",
   "patches": {
-    "marisa_silent_laser": "Makes Marisa's laser silent in th12 and above."
+    "marisa_silent_laser": "Makes Marisa's laser silent in th12 and above.",
+	  "th17_marisa_silent_missile": "Makes Marisa's missiles silent in th17 ONLY."
   },
   "servers": [
     "https://mirrors.thpatch.net/LmocinemodPatchRepo/"
---
+++ th17_marisa_silent_missile/patch.js
@@ -0,0 +1,10 @@
+{
+  "id": "th17_marisa_silent_missile",
+  "servers": [
+    "https://mirrors.thpatch.net/LmocinemodPatchRepo/th17_marisa_silent_missile/"
+  ],
+  "title": "Makes Marisa's missiles silent in th17 ONLY.",
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ]
+}
